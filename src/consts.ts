52914b377f90fe2e24f1902822df555f5d5a7129
A-mont <a.mont.24.777@gmail.com>
2025-05-08 21:32:23 -0600

    feat(const): Add signless constants

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,7 @@ const ADDRESS = {
   NODE: import.meta.env.VITE_NODE_ADDRESS as string,
 };
 
+export const ALLOWED_SIGNLESS_ACTIONS = ['createNewBattle', 'registration', 'startBattle', 'makeMove'];
+
+
 export { ADDRESS };
